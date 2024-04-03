423f4f1a109d8f2601e8bd7ae533803ba412ec5d
hassan2112d <hassan2112d@aptechgdn.net>
2024-04-03 07:57:55 +0500

    new porfolio

---
+++ src/data/constants.js
@@ -0,0 +1,438 @@
+export const Bio = {
+  name: "M.Hassan Rajani",
+  roles: [
+    "Full Stack Developer",
+    "Backend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/hassan2112d",
+  resume:
+    "https://hassan2112d.github.io/images/M.Hassan%20ResumeCv.pdf",
+  linkedin: "https://linkedin.com/in/hassan-rajani-84bb8722a/",
+  insta: "https://www.instagram.com/hassann_.1/",
+  facebook: "https://www.facebook.com/profile.php?id=100069025241503",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Angular TS",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/angular/angular-original.svg",
+      },
+      
+     
+    
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      
+      {
+         name : "PHP" ,
+         image :
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "SQL SERVER",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/microsoftsqlserver/microsoftsqlserver-original.svg",
+      },
+      {
+        name: "Laravel",
+        image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-original.svg",
+      },
+      {
+        name: "ASP.NET",
+        image: "https://www.vectorlogo.zone/logos/dotnet/dotnet-horizontal.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+      
+      {
+        name: "Figma",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+      },
+      {
+        name: "LINUX",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.edusuite.pk/wp-content/uploads/2022/06/A-List-of-Best-Online-Teaching-Websites-in-Pakistan.jpg",
+    role: "Programming Instructor",
+    company: "Personal Online Teacher",
+    date: "Jan 2024 - Present",
+    desc: "Welcome to my portfolio as a Programming Instructor and Personal Online Teacher, Through one-on-one online sessions, I guide students in mastering Programming intricacies . Join me on this journey of digital enlightenment, where I combine my passion for coding with my commitment to fostering a new generation of skilled developers.  (Enroll now for just $65 and unlock the gateway to a rewarding career in web development).  Contact at : +923149922774 / hassan2112d@aptechgdn.net .",
+    skills: [
+      "PHP",
+      "LARAVEL",
+      "Html",
+      "Bootstrap",
+      "Css",
+      "Javascript",
+      "react",
+      "Python",
+      "Angular",
+      "Asp.net",
+      "mongodb",
+      "mysql",
+      "sqlserver",
+      "Github",
+      
+    ],
+    doc: "",
+  },
+    {
+    id: 0,
+    img: "https://wenawa.com/wp-content/uploads/2023/06/logo.png",
+    role: "BACKEND DEVELOPER",
+    company: "WENAWA",
+    date: "FEB 2024 - Present",
+    desc: "As a Laravel Developer, I develop and maintain web applications using Laravel framework, ensuring high-quality, scalable solutions, collaborating closely with a skilled team.",
+    skills: [
+      "PHP",
+      "LARAVEL",
+      "FlUTTER",
+      "APIS",
+      "FIREBASE",
+      "MYSQL",
+      
+    ],
+    doc: "",
+  },
+  {
+    id: 2,
+    img: "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=608,fit=crop,q=95/Aq20eV79zLfpXV6b/logo-png-mnl7npnlXjHPl9KV.png",
+    role: "PYTHON GUI INTERN",
+    company: "CODSOFT",
+    date: "OCT 2023 - Nov 2023",
+    desc: "As a Python GUI Intern, I have gained valuable experience in developing graphical user interfaces (GUIs) using Python frameworks such as Tkinter. Throughout my internship, I have actively contributed to designing and implementing user-friendly interfaces for desktop applications.",
+    skills: [
+      "Tkinter",
+      "GUI",
+      "Python",
+      "Debugging",
+      "Logical Thinking",
+      
+      
+    ],
+    doc: "",
+  },
+  
+  {
+    id: 4,
+    img: "https://scontent.fkhi2-2.fna.fbcdn.net/v/t39.30808-6/302003764_509679384490548_8471908078951877025_n.png?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=iXJkVVi0_VkAX_juKvI&_nc_ht=scontent.fkhi2-2.fna&oh=00_AfDxgF9_BAqagpOtpcQGSMouM51tsqgA51srkv60NzGTuQ&oe=66115396",
+    role: "Core PHP Developer",
+    company: "TRAFFIC BUILDER.BIZ",
+    date: "March 2023 - OCT 2023",
+    desc: "As a PHP Developer, I have honed my skills in designing, developing, and maintaining web applications. I possess a solid understanding of PHP programming language.Throughout my career, I have successfully contributed to various projects, collaborating with cross-functional teams to deliver high-quality solutions that align with business objectives. ",
+    skills: [
+      "Core PHP",
+      "Wordpress",
+      "Plugin",
+      "Debugging",
+      "MYSQL",
+      
+      
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT395MR_ILJvXShvN_xMHUf6Rtr8rH-PDjdtaXfDIp-qg&s",
+    school: "GOOGLE COURSERA",
+    date: "March 2024 ",
+   
+    degree: "Introduction of GITHUB ",
+  },
+  {
+    id: 0,
+    img: "https://uok.edu.pk/images/logo.png",
+    school: "Karachi University",
+    date: "JAN 2023 - PRESENT ",
+   
+    desc: "Obtaining a Bachelor's in Computer Science from the University of Karachi represents a dedication to academic excellence and a commitment to mastering the fundamentals of computer science. ",
+    degree: "BACHOLORS IN COMPUTER SCIENCE ",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWhgJJG9oTxy-VPIWn3TPLf8usxWa_fFVMTCHAta8obA&s",
+    school: "LINKDEIN LEARNING",
+    date: "JAN 2023 - PRESENT ",
+   
+    degree: "Career Essentials in Generative AI by Microsoft and LinkedIn ",
+  },
+  {
+    id: 1,
+    img: "https://aptech-education.com.pk/assets/images/logo.png",
+    school: "Aptech Learning Center",
+    date: "NOV 2021 - PRESENT",
+    
+    desc: "Enrolled in the Advanced Diploma in Software Engineering (ADSE) program at Aptech Learning Center, I am actively pursuing a comprehensive education in software development. ",
+    degree: "ADSE 3 YEARS DIPLOMA",
+  },
+  {
+    id: 4,
+    img: "https://www.ncr-cet.com/wp-content/uploads/2023/01/logo.webp",
+    school: "NCR-CET COLLEGE",
+    date: "Apr 2020 - Apr 2022",
+    
+    degree: "Intermediate HSC in Computer Science",
+  },
+  {
+    id: 5,
+    img: "https://school.greenislandtrust.org/img/Logo.png",
+    school: "GREEN ISLAND FOUNDATION SCHOOL -- GIFS",
+    date: "Apr 2018 - Apr 2020",
+    
+    degree: "Matriculation SSC in Computer Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Welcome to Daily Shop (E-Project)",
+    description:
+      "Welcome to Daily Shop is a dynamic web application built using Laravel and PHP, offering users a seamless shopping experience. With an intuitive user interface, users can browse through a wide range of products, add items to their cart, and securely complete their purchases.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/e-project.png",
+    tags: [
+      "Mysql",
+      "PHP",
+      "Laravel",
+      "Html",
+      "CSS",
+      "Bootstrap",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/hassan2112d/E-Project-Daily-Shop-Website-2nd-Sem-.git",
+    
+  },
+  {
+    id: 0,
+    title: "MRASPORTS",
+    description:
+      "Built on a foundation of expertise in Custom PHP, MySQL, WordPress, JavaScript, and E-Commerce, MRASPORTS delivers a comprehensive and immersive online shopping experience. Our platform boasts a user-friendly interface designed to facilitate seamless navigation and efficient browsing across a diverse range of sports products.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(17).png",
+    tags: ["Custom PHP", "mysql", "Wordpress", "Javascript", "E-Commerce"],
+    category: "web app",
+    github: "https://mrasports.pk",
+    
+  },
+  {
+    id: 1,
+    title: "GIWW",
+    description:
+      "Crafted with expertise in Custom PHP, Core PHP, HTML, MySQL, CSS, Bootstrap, and Slick Slider, GIWW embodies innovation and functionality. Our platform delivers a seamless and immersive learning environment, enabling women to explore Islamic teachings and develop practical skills at their own pace.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(14).png",
+    tags: [
+      "Custom PHP",
+      "Core PHP",
+      "HTML",
+      "mysql",
+      "CSS",
+      "Bootstrap",
+      "Slick Slider",
+    ],
+    category: "web app",
+    github: "https://giww.greenislandtrust.org/",
+  },
+  {
+    id: 2,
+    title: "CODEIX",
+    description:
+      "Crafted with expertise in PHP, Core PHP, HTML, Bootstrap, MySQL, and Object-Oriented Programming (OOP), CODEIX is more than just a blogging platform—it's a dynamic showcase of innovation and functionality. Our meticulously designed interface provides an intuitive and immersive blogging experience, empowering users to unleash their creativity and connect with audiences worldwide.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(18).png",
+    tags: ["PHP", "Core PHP", "HTML", "Bootstrap", "mysql", "OOP"],
+    category: "web app",
+    github: "https://github.com/hassan2112d/Advance_PHP_Practise.git",
+    
+  },
+  
+  
+  
+  {
+    id: 5,
+    title: "GIP",
+    description:
+    "Welcome to GIP, your premier destination for literary enthusiasts seeking to delve into the captivating world of books. Immerse yourself in a digital realm where imagination knows no bounds and the written word comes to life.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(15).png",
+    tags: ["Custom PHP",
+    "Core PHP",
+    "HTML",
+    "mysqli",
+    "CSS",
+    "Bootstrap",
+    "Slick Slider",],
+    category: "web app",
+    github: "https://publications.greenislandtrust.org/",
+    },
+    {
+      id: 5,
+      title: "Todo Tasks",
+      description:
+        " A Todo Web App made with Tkinter containing 4 Tasks. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+      image:
+        "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(12).png",
+      tags: [
+      "PYTHON",
+      "Tkinter",
+      "GUI",
+      ],
+      category: "web app",
+      github: "https://github.com/hassan2112d/CODSOFT.git",
+      },
+    {
+    id: 5,
+    title: "Yummy Food Court",
+    description:
+    "At Yummy Food Center, we're committed to delivering exceptional service and culinary excellence. Leveraging the power of Firebase, we ensure secure transactions, timely delivery, and personalized customer support, so you can enjoy your meal with peace of mind.",
+    image:
+      "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%20(13).png",
+    tags: ["TS",
+    "Boostrap",
+    "HTML",
+    "Angular TS",
+    "firebase",
+    
+    ],
+    category: "web app",
+    },
+    {
+      id: 5,
+      title: "Sailor ",
+      description:
+      "Join us on this voyage of innovation and discovery with Sailor. Whether you're charting new territories in the digital landscape or navigating the challenges of modern business, let Sailor be your compass as you set sail towards success.",
+      image:
+        "https://raw.githubusercontent.com/hassan2112d/Images/main/Screenshot%202024-04-02%20171710.png",
+      tags: ["Asp.net",
+      "Backend",
+      ".net",
+      "Bootstrap",
+      "sqlserver",
+      
+      ],
+      category: "web app",
+      },
+   
+  
+  {
+    id: 7,
+    title: "QuizLab",
+    description:
+      "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+    image:
+      "",
+    tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+    category: "android app",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2021, text: "Started my journey" },
+  { year: 2022, text: "Creating Projects" },
+  { year: 2023, text: "Founded JavaScript Mastery" },
+  { year: 2023, text: "Shared my projects with the world" },
+  { year: 2024, text: "Started my own platform" },
+];
